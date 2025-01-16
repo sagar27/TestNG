5361660eebe943949582ccea7596b806f2783cc0
sagar <sagar@192.168.1.37>
2025-01-16 20:11:05 +0530

    f

--- TestNG_framework/src/one/Testing.java
+++ TestNG_framework/src/one/Testing.java
@@ -3,13 +3,13 @@ package one;
 import org.testng.annotations.Test;
 
 public class Testing {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		
-//		System.out.println("inside main");
-//
-//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("inside main");
+
+	}
 	@Test
 	public void testngmethod(){
 		System.out.println("inside testngmethod");
