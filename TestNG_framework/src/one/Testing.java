8ef0eadcf445afb7dd898b191ddc7bc9f38059bb
sagar <sagar@192.168.1.37>
2025-01-16 20:04:47 +0530

    configured to TestNG from java project

---
+++ TestNG_framework/src/one/Testing.java
@@ -0,0 +1,18 @@
+package one;
+
+import org.testng.annotations.Test;
+
+public class Testing {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		System.out.println("inside main");
+//
+//	}
+	@Test
+	public void testngmethod(){
+		System.out.println("inside testngmethod");
+	}
+
+}
